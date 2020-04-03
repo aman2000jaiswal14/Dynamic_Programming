b2be618ccd21dc1d299f46b9990dfd412fc4bc1e
AMAN JAISWAL <aman2000jaiswal14@gmail.com>
2020-04-03 20:21:02 +0530

    Add files via upload

---
+++ sample_code/solving_structure.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<string>
+#include<cstdlib>
+using namespace std;
+#define pb push_back
+#define ll long long
+#define boost ios_base::sync_with_stdio(false);\
+			cin.tie(NULL);
+			
+ll MAXIMUM(ll a,ll b)
+{return a>=b?a:b;}
+
+ll MINIMUM(ll a,ll b)
+{return a<b?a:b;}	
+
+int main()
+{	boost;
+	ll t;
+	cin>>t;
+	for(int tt=0;tt<t;tt++)
+	{
+		
+	}
+	return 0;
+}
+
+//LOOP CODE 1
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+		
+		}
+	}
+
+// LOOP CODE 2
+
+		for(int i=0;i<n;i++)
+		{		
+			for(int j=0;j<n;j++)
+			{
+		
+			}
+		}
