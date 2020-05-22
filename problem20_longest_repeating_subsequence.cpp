40522219ae0152a49b9dc2fd4fcf93cf719d0892
AMAN JAISWAL <aman2000jaiswal14@gmail.com>
2020-05-22 10:49:33 +0530

    LONGEST REPEATING SUBSEQUENCE

---
+++ problem20_longest_repeating_subsequence.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	string a;
+	cin>>a;
+	
+	//copy  string a to b
+	
+	string b(a);
+	
+	int dp[n+1][n+1];
+	
+	for(int i=0;i<n+1;i++)
+	{
+		dp[0][i]=0;
+		dp[i][0]=0;
+	}
+	
+	for(int i=1;i<n+1;i++)
+	{
+		for(int j=1;j<n+1;j++)
+		{
+			if(a[i-1]==b[j-1] && i!=j)  //extra condition
+				dp[i][j]=dp[i-1][j-1]+1;
+			else
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+					
+		}
+	}
+	
+	cout<<dp[n][n];
+	
+	return 0;
+	
+}
