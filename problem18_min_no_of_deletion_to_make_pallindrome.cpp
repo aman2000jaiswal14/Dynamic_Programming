e7840dfa88dee20297bd274fbf6e70c0bdcde585
AMAN JAISWAL <aman2000jaiswal14@gmail.com>
2020-05-22 10:03:09 +0530

    MIN NUMBER OF DELETION TO MAKE PALLINDROME

---
+++ problem18_min_no_of_deletion_to_make_pallindrome.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	string a;
+	cin>>a;
+	string b(a);
+	reverse(a.begin(),a.end());
+	
+	int dp[n+1][n+1];
+	
+	for(int i=0;i<n+1;i++)
+	{
+		dp[i][0]=0;
+		dp[0][i]=0;
+	}
+	
+	for(int i=1;i<n+1;i++)
+	{
+		for(int j=1;j<n+1;j++)
+		{
+			if(a[i-1]==b[j-1])
+			{
+				dp[i][j]=dp[i-1][j-1]+1;
+			}
+			else
+			dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		}
+	
+	}
+	
+	cout<<n-dp[n][n];
+	return 0;
+}
